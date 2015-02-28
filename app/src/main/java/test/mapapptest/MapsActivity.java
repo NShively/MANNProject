c62718334902cf6e3ba668ebd5059cc489906121
NShively <njshively1354@gmail.com>
2015-02-27 21:29:11 -0800

    Added current location, as well as support to access current location

--- app/src/main/java/test/mapapptest/MapsActivity.java
+++ app/src/main/java/test/mapapptest/MapsActivity.java
@@ -1,38 +1,77 @@
 package test.mapapptest;
 
-import android.content.Context;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 
-import com.google.android.gms.common.GooglePlayServicesClient;
+
+import android.app.Dialog;
+import android.content.IntentSender;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import android.content.Intent;
+import android.content.IntentSender;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-public class MapsActivity extends FragmentActivity {
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class MapsActivity extends FragmentActivity implements
+        LocationListener, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
+    //implements com.google.android.gms.location.LocationListener, GoogleApiClient.ConnectionCallbacks,
+    //GoogleApiClient.OnConnectionFailedListener
+    private static final long ONE_MIN = 1000 * 60;
+    private static final long TWO_MIN = ONE_MIN * 2;
+    private static final long FIVE_MIN = ONE_MIN * 5;
+    private static final long POLLING_FREQ = 1000 * 30;
+    private static final long FASTEST_UPDATE_FREQ = 1000 * 5;
+    private static final float MIN_ACCURACY = 25.0f;
+    private static final float MIN_LAST_READ_ACCURACY = 500.0f;
+    private final static int REQUEST_RESOLVE_ERROR = 1001;
+
+    private LocationRequest mLocationRequest;
+    private Location mBestReading;
+    private GoogleApiClient mGoogleApiClient;
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
 
-    }
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        mLocationRequest.setInterval(POLLING_FREQ);
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_FREQ);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+    }
+/*
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
-    }
+    }*/
 
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
@@ -55,6 +94,7 @@ public class MapsActivity extends FragmentActivity {
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
+            mMap.setMyLocationEnabled(true);
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
@@ -70,16 +110,192 @@ public class MapsActivity extends FragmentActivity {
      */
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-
-        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = service.getBestProvider(criteria, false);
-        Location location = service.getLastKnownLocation(provider);
+       // Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        //Location here = mMap.getMyLocation();
+       // LatLng userLocation = new LatLng(here.getLatitude(),here.getLongitude());
+        //LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+      //  Criteria criteria = new Criteria();
+     //   String provider = service.getBestProvider(criteria, false);
+      //  Location location = service.getLastKnownLocation(provider);
          //Both are returned as NULL. Hmm.
-        LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+      //  LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+
+        //mMap.addMarker(new MarkerOptions().position(userLocation).title("I'm here!"));
+    }
+
+    //------------//------------//---------//----
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
 
 
-        mMap.addMarker(new MarkerOptions().position(userLocation).title("I'm here!"));
+        super.onResume();
+
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }setUpMapIfNeeded();
+    }
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
+
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!mGoogleApiClient.isConnecting() &&
+                        !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+
+    }
+
+    private boolean servicesAvailable() {
+
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (ConnectionResult.SUCCESS == resultCode) {
+            return true;
+        }
+        else {
+            GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0).show();
+            return false;
+        }
+    }
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // TODO Auto-generated method stub
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this,
+                        REQUEST_RESOLVE_ERROR);
+				/*
+				 * Thrown if Google Play services canceled the original
+				 * PendingIntent
+				 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+			/*
+			 * If no resolution is available, display a dialog to the user with
+			 * the error.
+			 */
+            Log.d("Connection Failed:", "" + connectionResult.getErrorCode()
+                    + " " + connectionResult.toString());
+            showErrorDialog(connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle dataBundle) {
+        // TODO Auto-generated method stub
+        // Get first reading. Get additional location updates if necessary
+        if (servicesAvailable()) {
+            // Get best last location measurement meeting criteria
+            mBestReading = bestLastKnownLocation(MIN_LAST_READ_ACCURACY, FIVE_MIN);
+            //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+            mMap.addMarker(new MarkerOptions().position(new LatLng(mBestReading.getLatitude(), mBestReading.getLongitude())));
+            if (null == mBestReading
+                    || mBestReading.getAccuracy() > MIN_LAST_READ_ACCURACY
+                    || mBestReading.getTime() < System.currentTimeMillis() - TWO_MIN) {
+
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+                // Schedule a runnable to unregister location listeners
+                Executors.newScheduledThreadPool(1).schedule(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, MapsActivity.this);
+                    }
+
+                }, ONE_MIN, TimeUnit.MILLISECONDS);
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // TODO Auto-generated method stub
+        // Determine whether new location is better than current best
+        // estimate
+        if (null == mBestReading || location.getAccuracy() < mBestReading.getAccuracy()) {
+            mBestReading = location;
+
+            if (mBestReading.getAccuracy() < MIN_ACCURACY) {
+                LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            }
+        }
+    }
+
+
+    private Location bestLastKnownLocation(float minAccuracy, long minTime) {
+        Location bestResult = null;
+        float bestAccuracy = Float.MAX_VALUE;
+        long bestTime = Long.MIN_VALUE;
+
+        // Get the best most recent location currently available
+        Location mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (mCurrentLocation != null) {
+            float accuracy = mCurrentLocation.getAccuracy();
+            long time = mCurrentLocation.getTime();
+
+            if (accuracy < bestAccuracy) {
+                bestResult = mCurrentLocation;
+                bestAccuracy = accuracy;
+                bestTime = time;
+            }
+        }
+
+        // Return best reading or null
+        if (bestAccuracy > minAccuracy || bestTime < minTime) {
+            return null;
+        }
+        else {
+            return bestResult;
+        }
+    }
+
+
+    private void showErrorDialog(int errorCode) {
+
+        // Get the error dialog from Google Play services
+        Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                errorCode,
+                this,
+                REQUEST_RESOLVE_ERROR);
+
+
+        // If Google Play services can provide an error dialog
+        if (errorDialog != null) {
+
+            errorDialog.show();
+        }
     }
 
 
