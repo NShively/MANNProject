86116542d4749416b1c971ea2b0d571eb5dc17bb
nathan <napaunderground@gmail.com>
2015-03-17 19:40:10 -0700

    set start-up to run input dialog.  Coded sample input boxes, corrected spelling of listener...

---
+++ app/src/main/java/test/mapapptest/Interface/IHttpCallbackListener.java
@@ -0,0 +1,9 @@
+package test.mapapptest.Interface;
+
+import test.mapapptest.Model.MileageModel;
+/**
+ * Created by nathan on 3/17/15.
+ */
+public class IHttpCallbackListener {
+   // public void onMileageCallback(MileageModel model);
+}
